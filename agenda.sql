09f4f6c2716abf81fd14d27a7f4e4ebfad818938
emanuelFE <87094010+emanuelFE@users.noreply.github.com>
2022-05-16 16:49:05 -0300

    Add files via upload

---
+++ agenda.sql
@@ -0,0 +1,17 @@
+/**
+  Agenda de contados
+  @author Emanoel Felipe da Silva
+  */
+  
+-- Verificar bancos de dados
+show databases;
+
+-- Criar um novo banco de dados
+create database teste;
+create database agendaEmanoel;
+
+-- Excluir um banco de dados
+drop database teste;
+
+-- Selecionar o banco de dados
+use agendaEmanoel; 
